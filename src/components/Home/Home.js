e51209d420c327371828caf84c7bb4797a7fcd86
Vibhuti <vibhuti6378@gmail.com>
2022-12-24 13:08:42 +0530

    added more changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,23 @@
+import React, { useEffect, useRef } from 'react';
 import "./HomeStyles.css";
 import homeSvg from "../../images/home.svg";
+import { Scrambler, Cycler } from "react-text-scrambler";
 // Default component below Navbar
 function Home() {
+    const phrases = [
+        'Technocracy',
+        'This is Technocracy',
+        'This is Magic...',
+    ];
+    
     return (<>
         <div style={{width:'100%',height:'100%'}}>
         <img src={homeSvg} width="100%" height="100%" alt="logo"/>
             <div className="container">
                 <div className="tag-container"></div>
-                <p className="tagline">THIS IS TECHNOCRACY</p>
+                <p className="tagline">
+                <Cycler duration={ 3000 } strings={ phrases } />
+                </p>
                 <p className="text-Container" >
 				The TechnoCracy is the students' technical committee of NIT Raipur. It
 				was a seed sown in 2007 by a few technical enthusiasts, which has grown
